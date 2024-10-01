01cb0304c923f01371015a47b1a10180c6f0f8c7
Trygve Tegnander <trygveteg@gmail.com>
2024-10-01 16:16:30 +0200

    Added Torjei's high def scheme

--- lax_friedrichs.jl
+++ lax_friedrichs.jl
@@ -23,8 +23,9 @@ f(x) = a*x
 J(x) = a
 
 U_friedrichs = lax_friedrichs(f, u_0, dx, dt, T)
-U_upwind = central_upwind(f, u_0, dx, dt, T)
+U_upwind = central_diff(f, u_0, dx, dt, T)
 U_wendroff = lax_wendroff(f, J, u_0, dx, dt, T)
+U_high_res = high_res_torjei(f, J, u_0, dx, dt, T)
 
 t = range(0, T, length=size(U_friedrichs, 1))
 # surface(x, t, U_friedrichs, title="Lax-Friedrichs")
@@ -34,4 +35,4 @@ t = range(0, T, length=size(U_friedrichs, 1))
 #plot_2d(x, t, U_friedrichs)
 #plot_2d(x, t, U_upwind)
 #plot_2d(x, t, U_wendroff)
-plot_2ds(x, t, [U_friedrichs, U_upwind, U_wendroff], ["Lax-Friedrichs", "Central Upwind", "Lax-Wendroff"])
+plot_2ds(x, t, [U_friedrichs, U_upwind, U_wendroff, U_high_res], ["Lax-Friedrichs", "Central diff", "Lax-Wendroff", "High res"])
--- scalar_solvers.jl
+++ scalar_solvers.jl
@@ -1,7 +1,8 @@
 module ScalarSolvers
     export lax_friedrichs
-    export central_upwind
+    export central_diff
     export lax_wendroff
+    export high_res_torjei
 
     function lax_friedrichs(f, u0, dx, dt, T)
         N = length(u0)
@@ -23,7 +24,7 @@ module ScalarSolvers
         return u
     end
 
-    function central_upwind(f, U_0, dx, dt, T)
+    function central_diff(f, U_0, dx, dt, T)
         N = length(U_0)
         M = ceil(Int, T/dt)
 
@@ -66,6 +67,53 @@ module ScalarSolvers
                     J_2 = J(0.5*(U[i-1, j-1] + U[i-1, j]))
                     U[i, j] = U[i-1, j] - 0.5*dt/dx*(f(U[i-1, j+1]) - f(U[i-1, j-1])) + 0.5*(dt/dx)^2*(J_1*(f(U[i-1, j+1])-f(U[i-1, j])) - J_2*(f(U[i-1, j] - f(U[i-1, j-1]))))
                 end
+            end
+        end
+        return U
+    end
+
+    function central_upwind(f, J, U_0, dx, dt, T)
+        N = length(U_0)
+        M = ceil(Int, T/dt)
+
+        U = fill(1.0, (M+1, N))
+        U[1, 1:N] = U_0
+        for i in 2:M+1
+            for j in 1:N
+                if j == 1
+                    U[i, j] = U[i-1, j] 
+                elseif j == N
+                    U[i, j] = U[i-1, j] 
+                else
+                    U[i, j] = U[i-1, j] 
+                end
+            end
+        end
+    end
+
+    function high_res_torjei(f, J, U_0, dx, dt, T)
+        N = length(U_0)
+        M = ceil(Int, T/dt)
+
+        U = fill(1.0, (M+1, N))
+        U[1, 1:N] = U_0
+        F_1 = 0.0
+        F_2 = 0.0
+        for i in 2:M+1
+            for j in 1:N
+                if j == 1
+                    F_1 = (f(U[i-1, N]) + f(U[i-1, j]))/2 - (max(abs(J(U[i-1, N])), abs(J(U[i-1, j]))/2))*(U[i-1, j] - U[i-1, N])
+                    F_2 = (f(U[i-1, j]) + f(U[i-1, j+1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, j+1]))/2))*(U[i-1, j+1] - U[i-1, j])
+
+                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
+                elseif j == N
+                    F_2 = (f(U[i-1, j]) + f(U[i-1, 1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, 1]))/2))*(U[i-1, 1] - U[i-1, j])
+                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
+                else
+                    F_2 = (f(U[i-1, j]) + f(U[i-1, j+1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, j+1]))/2))*(U[i-1, j+1] - U[i-1, j])
+                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
+                end
+                F_1 = F_2
             end
         end
         return U
