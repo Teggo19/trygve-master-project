9646b9234f70ff75c4e54a1f7482ebf0043b4541
Trygve Tegnander <trygveteg@gmail.com>
2024-10-22 19:31:31 +0200

    Rewrote the solvers for easier implementation of new solvers

--- scalar_solvers.jl
+++ scalar_solvers.jl
@@ -2,27 +2,61 @@ module ScalarSolvers
     export lax_friedrichs
     export central_diff
     export lax_wendroff
-    export low_res_torjei
+    export upwind
 
-    function lax_friedrichs(f, u0, dx, dt, T)
-        N = length(u0)
+    function finite_volume_solver(f, J, U_0, dx, dt, T, flux, periodic_boundary)
+        N = length(U_0)
         M = ceil(Int, T/dt)
         u = zeros(M+1, N)
-        u[1, :] = u0
+        u[1, :] = U_0
         for i in 2:M+1
             for j in 1:N
-                if j == 1
-                    u[i, j] = 0.5*(u[i-1, 2] + u[i-1, N]) - 0.5*dt/dx*(f(u[i-1, 2]) - f(u[i-1, N]))
-                elseif j == N
-                    u[i, j] = 0.5*(u[i-1, 1] + u[i-1, N-1]) - 0.5*dt/dx*(f(u[i-1, 1]) - f(u[i-1, N-1]))
+                if periodic_boundary
+                    if j == 1
+                        u[i, j] = u[i-1, j] - dt/dx*(flux(f, J, u[i-1, j], u[i-1, j+1]) - flux(f, J, u[i-1, N], u[i-1, j]))
+                    elseif j == N
+                        u[i, j] = u[i-1, j] - dt/dx*(flux(f, J, u[i-1, j], u[i-1, 1]) - flux(f, J, u[i-1, j-1], u[i-1, j]))
+                    else
+                        u[i, j] = u[i-1, j] - dt/dx*(flux(f, J, u[i-1, j], u[i-1, j+1]) - flux(f, J, u[i-1, j-1], u[i-1, j]))
+                    end
                 else
-                    u[i, j] = 0.5*(u[i-1, j+1] + u[i-1, j-1]) - 0.5*dt/dx*(f(u[i-1, j+1]) - f(u[i-1, j-1]))
-            
+                    if j == 1 || j == N
+                        u[i, j] = u[i-1, j]
+                    else
+                        u[i, j] = u[i-1, j] - dt/dx*(flux(f, J, u[i-1, j], u[i-1, j+1]) - flux(f, J, u[i-1, j-1], u[i-1, j]))
+                    end
                 end
             end
         end
         return u
     end
+
+    function lax_friedrichs(f, U_0, dx, dt, T, periodic_boundary)
+        function flux(f, J, u1, u2)
+            return 0.5*dx/dt*(u1-u2) + 0.5*(f(u1) + f(u2))
+        end
+
+        return finite_volume_solver(f, 0, U_0, dx, dt, T, flux, periodic_boundary)
+    end
+
+    function upwind(f, J, U_0, dx, dt, T, periodic_boundary)
+        function flux(f, J, u1, u2)
+            if J(u1) >= 0
+                return f(u1)
+            else
+                return f(u2)
+            end
+        end
+        return finite_volume_solver(f, J, U_0, dx, dt, T, flux, periodic_boundary)
+    end
+
+    function lax_wendroff(f, U_0, dx, dt, T, periodic_boundary)
+        function flux(f, J, u1, u2)
+            return f((0.5*(u1 + u2)) - 0.5*dt/dx*(f(u2) - f(u1)))
+        end
+        return finite_volume_solver(f, 0, U_0, dx, dt, T, flux, periodic_boundary)
+    end
+
 
     function central_diff(f, U_0, dx, dt, T)
         N = length(U_0)
@@ -44,80 +78,5 @@ module ScalarSolvers
         end
         return U
     end
-
-    function lax_wendroff(f, J, U_0, dx, dt, T)
-        N = length(U_0)
-        M = ceil(Int, T/dt)
-
-        U = fill(1.0, (M+1, N))
-        U[1, 1:N] = U_0
-        u_1 = 0.0
-        u_2 = 0.0
-        for i in 2:M+1
-            for j in 1:N
-                if j == 1
-                    u_1 = 0.5*(U[i-1, N] + U[i-1, j]) - 0.5*dt/dx*(f(U[i-1, j]) - f(U[i-1, N]))
-                    u_2 = 0.5*(U[i-1, j] + U[i-1, j+1]) - 0.5*dt/dx*(f(U[i-1, j+1]) - f(U[i-1, j]))
-                    
-                elseif j == N
-                    u_2 = 0.5*(U[i-1, j] + U[i-1, 1]) - 0.5*dt/dx*(f(U[i-1, 1]) - f(U[i-1, j]))
-
-                else
-                    u_2 = 0.5*(U[i-1, j] + U[i-1, j+1]) - 0.5*dt/dx*(f(U[i-1, j+1]) - f(U[i-1, j]))
-                    
-                end
-                U[i, j] = U[i-1, j] - dt/dx*(f(u_2) - f(u_1))
-                u_1 = u_2
-            end
-        end
-        return U
-    end
-
-    function central_upwind(f, J, U_0, dx, dt, T)
-        N = length(U_0)
-        M = ceil(Int, T/dt)
-
-        U = fill(1.0, (M+1, N))
-        U[1, 1:N] = U_0
-        for i in 2:M+1
-            for j in 1:N
-                if j == 1
-                    U[i, j] = U[i-1, j] 
-                elseif j == N
-                    U[i, j] = U[i-1, j] 
-                else
-                    U[i, j] = U[i-1, j] 
-                end
-            end
-        end
-    end
-
-    function low_res_torjei(f, J, U_0, dx, dt, T)
-        N = length(U_0)
-        M = ceil(Int, T/dt)
-
-        U = fill(1.0, (M+1, N))
-        U[1, 1:N] = U_0
-        F_1 = 0.0
-        F_2 = 0.0
-        for i in 2:M+1
-            for j in 1:N
-                if j == 1
-                    F_1 = (f(U[i-1, N]) + f(U[i-1, j]))/2 - (max(abs(J(U[i-1, N])), abs(J(U[i-1, j]))/2))*(U[i-1, j] - U[i-1, N])
-                    F_2 = (f(U[i-1, j]) + f(U[i-1, j+1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, j+1]))/2))*(U[i-1, j+1] - U[i-1, j])
-
-                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
-                elseif j == N
-                    F_2 = (f(U[i-1, j]) + f(U[i-1, 1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, 1]))/2))*(U[i-1, 1] - U[i-1, j])
-                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
-                else
-                    F_2 = (f(U[i-1, j]) + f(U[i-1, j+1]))/2 - (max(abs(J(U[i-1, j])), abs(J(U[i-1, j+1]))/2))*(U[i-1, j+1] - U[i-1, j])
-                    U[i, j] = U[i-1, j] - dt/dx*(F_2 - F_1)
-                end
-                F_1 = F_2
-            end
-        end
-        return U
-    end
-
+ 
 end
